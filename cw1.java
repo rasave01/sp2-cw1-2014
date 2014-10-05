8022b97f64ca5237fd487633152b868f032bb3d1
= <rasave01@mail.bbk.ac.uk>
2014-10-05 23:24:36 +0100

    added methods for reading + writting arrays, not working prop

---
+++ cw1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class cw1 {
+
+	public static void main(String[] args) {
+		// declare names of arrays as constants
+		final char a='1';
+		final char b='2';
+		/** fill arrays in turn by getting first element and if not 0 continue to get elements until 0 */
+		int first[] = fillArray(a);
+		//int second[] = fillArray(b);
+		printArray(a, first);
+		
+		/** method to find common data and put it in a third array*/
+		/** method to display third array, and its length */
+		//changed something here to check git1
+		
+		/** method to find non-common values between two arrays, do this twice for 1+3, and 2+3*/
+	}
+	
+	
+	//method to read an array
+	private static int[] fillArray(char arrName) {
+		int[] returnArray;// declare array variable
+		returnArray = new int[100]; // initialise array
+		Scanner scanner;
+		scanner = new Scanner(System.in);
+		// get first element
+		int element = 1;
+		int i=0;
+		while (element!=0){
+			System.out.println("Enter data for array " + arrName +" (0 to finish): ");
+			element = scanner.nextInt();
+			returnArray[i]=element;
+			i++;
+			} ;
+		scanner.close();
+		return returnArray;
+		}
+	
+	//method to write non 0 values of an array
+	private static void printArray(char arrName, int[] array){
+		System.out.print("Values for array " + arrName + " is: ");
+		for (int i=0; i< array.length; i++){
+			// can't find another way of comparing with 0!!!
+			if((array[i]*1)!=array[i]){
+				System.out.println("\n");
+				return;
+			}else{
+				System.out.print(array[i]+" ");
+			}
+		}
+		System.out.println("\n");
+		return;
+	}
+
+
+}
